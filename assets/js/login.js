e08dda61fa68c8d18d097289cbdcfc74a2429af8
Perezpalenciaandres@gmail.com <Perezpalenciaandres@gmail.com>
2023-12-03 20:34:32 -0500

    implementación de carpeta de contenidos y js con pagina de apuntes

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,18 @@
-const btn = document.getElementById('boton')
-if (btn) {
-    btn.addEventListener('click', (Event) =>{
+const form = document.getElementById('form-login')
+if (form) {
+    form.addEventListener('submit', (Event) =>{
         Event.preventDefault()
+        const email = Event.target.elements.email.value;
+        const password = Event.target.elements.password.value;
+        console.log(email);
+
+        console.log(password);
+
+        // email = apuntes@gmail.com password = iurisprudences
+        if (email == 'apuntes@gmail.com' && password =='iurisprudences'){
+            localStorage.setItem('login','true') 
+            window.location = '../views/Apuntes.html'  
+        }
+       
     })
 }
